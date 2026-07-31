17f65c92a8c774cded0d982b4d8e6bea22e49ab1
aBotHamm <abrahamdiaz.office@gmail.com>
2026-07-31 14:57:18 -0700

    test

---
+++ src/chapter01/Exercise01_11.java
@@ -0,0 +1,28 @@
+package chapter01;
+
+public class Exercise01_11 {
+    public static void main() {
+        double start = 312032486;
+
+        // 1 birth every 7 seconds
+        // 1 death every 13 seconds
+        // 1 new immigrant every 45 seconds
+
+        //calculate how many seconds in a year
+
+        double secondsInOneYear = 60 * 60 * 24 * 365;
+        double birthsPerYear = secondsInOneYear / 7.0;
+        double deathsPerYear = secondsInOneYear / 13.0;
+        double immigrantsPerYear = secondsInOneYear / 45.0;
+
+        System.out.println(birthsPerYear);
+        System.out.println(deathsPerYear);
+        System.out.println(immigrantsPerYear);
+
+        for (int i = 0; i < 4 ; i++) {
+            start += birthsPerYear + deathsPerYear + immigrantsPerYear;
+            System.out.println("Population = " + start);
+        }
+
+    }
+}
